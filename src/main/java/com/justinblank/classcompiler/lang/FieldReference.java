b6d60178039502d3322e4c1a091fcf236e272ff1
Justin Blank <justin.blank@gmail.com>
2022-03-27 11:51:49 -0400

    Argument validation

--- src/main/java/com/justinblank/classcompiler/lang/FieldReference.java
+++ src/main/java/com/justinblank/classcompiler/lang/FieldReference.java
@@ -1,5 +1,9 @@
 package com.justinblank.classcompiler.lang;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Objects;
+
 public class FieldReference implements Expression {
 
     public final String fieldName;
@@ -7,8 +11,13 @@ public class FieldReference implements Expression {
     public final Type type;
 
     FieldReference(String fieldName, Type type, Expression expression) {
+        if (StringUtils.isBlank(fieldName)) {
+            throw new IllegalArgumentException("FieldName cannot be blank");
+        }
         this.fieldName = fieldName;
+        Objects.requireNonNull(type, "Type cannot be null");
         this.type = type;
+        Objects.requireNonNull(expression, "Expression cannot be null");
         this.expression = expression;
     }
 }
