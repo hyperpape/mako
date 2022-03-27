8906070b120b87bc2bc344ce7de81bf929f629de
Justin Blank <justin.blank@gmail.com>
2022-03-27 18:16:28 -0400

    Update ReferenceType to have toString, equals and hashCode methods

--- src/main/java/com/justinblank/classcompiler/lang/ReferenceType.java
+++ src/main/java/com/justinblank/classcompiler/lang/ReferenceType.java
@@ -1,5 +1,9 @@
 package com.justinblank.classcompiler.lang;
 
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.Objects;
+
 public class ReferenceType implements Type {
 
     public Type type() {
@@ -9,6 +13,9 @@ public class ReferenceType implements Type {
     public final String typeString;
 
     ReferenceType(String type) {
+        if (StringUtils.isBlank(type)) {
+            throw new IllegalArgumentException("Cannot create a reference Type with a blank type string");
+        }
         this.typeString = type;
     }
 
@@ -18,5 +25,25 @@ public class ReferenceType implements Type {
 
     public static ReferenceType of(String className) {
         return new ReferenceType(className);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ReferenceType that = (ReferenceType) o;
+        return Objects.equals(typeString, that.typeString);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(typeString);
+    }
+
+    @Override
+    public String toString() {
+        return "ReferenceType{" +
+                "typeString='" + typeString + '\'' +
+                '}';
     }
 }
