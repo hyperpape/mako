dfc7917db19e4dc354bb5c1476380e0f49dffe42
Justin Blank <justin.blank@gmail.com>
2022-04-17 00:51:53 -0400

    Add error checking to call constructor
    Immediately report error when calling an instance method without a receiver

--- src/main/java/com/justinblank/classcompiler/lang/Call.java
+++ src/main/java/com/justinblank/classcompiler/lang/Call.java
@@ -11,6 +11,9 @@ public class Call implements Expression {
     public final String className;
 
     Call(String className, String methodName, Type returnType, boolean isStatic, boolean isSpecial, boolean isInterface, Expression... arguments) {
+        if (!isStatic && arguments.length == 0) {
+            throw new IllegalArgumentException("Cannot call an instance method without a receiver");
+        }
         this.className = className;
         this.methodName = methodName;
         this.arguments = arguments;
