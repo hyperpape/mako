f751ff3a7e575553086ae4d6aada815d8196516c
Justin Blank <justin.blank@gmail.com>
2022-08-28 17:55:08 -0400

    Rudimentary graphviz support

---
+++ src/main/java/com/justinblank/classcompiler/GraphUtil.java
@@ -0,0 +1,25 @@
+package com.justinblank.classcompiler;
+
+public class GraphUtil {
+
+    public static String methodVis(Method method) {
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("digraph G {");
+        for (var b : method.blocks) {
+            for (var o : b.operations) {
+                if (o.inst == Operation.Inst.JUMP) {
+                    sb.append("\n");
+                    sb.append(b.number).append(" -> ").append(o.target.number).append(";");
+                }
+            }
+            if (b.number < method.blocks.size() - 1) {
+                sb.append("\n");
+                sb.append(b.number).append(" -> ").append(b.number + 1);
+            }
+        }
+
+        sb.append("}");
+        return sb.toString();
+    }
+}
