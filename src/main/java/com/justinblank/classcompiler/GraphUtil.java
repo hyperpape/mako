76a16b05ca771d4b4f376f9a7944875480b93491
Justin Blank <justin.blank@gmail.com>
2022-08-29 22:24:57 -0400

    Updates to more concisely print methods
    Suppress printing empty blocks

--- src/main/java/com/justinblank/classcompiler/GraphUtil.java
+++ src/main/java/com/justinblank/classcompiler/GraphUtil.java
@@ -1,5 +1,8 @@
 package com.justinblank.classcompiler;
 
+import static com.justinblank.classcompiler.Operation.Inst.JUMP;
+import static net.bytebuddy.jar.asm.Opcodes.GOTO;
+
 public class GraphUtil {
 
     public static String methodVis(Method method) {
@@ -7,20 +10,40 @@ public class GraphUtil {
 
         sb.append("digraph G {");
         for (var b : method.blocks) {
+            if (b.isEmpty() && b.number < method.blocks.size() - 1) {
+                continue;
+            }
             for (var o : b.operations) {
-                if (o.inst == Operation.Inst.JUMP) {
+                if (o.inst == JUMP) {
                     sb.append("\n");
-                    sb.append(b.number).append(" -> ").append(o.target.number);
+                    sb.append(b.number).append(" -> ").append(actualTarget(method, o.target.number).number  );
                     sb.append("[ label = ").append(ClassPrinter.getRepresentation(o)).append(" ]").append(";");
                 }
             }
             if (b.number < method.blocks.size() - 1) {
+                var op = b.operations.get(b.operations.size() - 1);
+                if (op.inst == JUMP && op.count == GOTO) {
+                    continue;
+                }
                 sb.append("\n");
-                sb.append(b.number).append(" -> ").append(b.number + 1);
+                sb.append(b.number).append(" -> ").append(actualTarget(method, b.number + 1).number);
             }
         }
 
         sb.append("}");
         return sb.toString();
+    }
+
+    public static Block actualTarget(Method method, int number) {
+        var target = method.blocks.get(number);
+        while (target.number < method.blocks.size() - 1) {
+            if (target.isEmpty()) {
+                target = method.blocks.get(target.number + 1);
+            }
+            else {
+                return target;
+            }
+        }
+        return target;
     }
 }
