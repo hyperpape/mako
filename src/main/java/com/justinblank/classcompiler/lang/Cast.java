ff7316f666340c3c38c14c224df7c0bd875d34da
Justin Blank <justin.blank@gmail.com>
2023-05-23 23:34:57 +0100

    Handle casts involving bytes

--- src/main/java/com/justinblank/classcompiler/lang/Cast.java
+++ src/main/java/com/justinblank/classcompiler/lang/Cast.java
@@ -33,8 +33,10 @@ public class Cast implements Expression {
                             return List.of(I2D);
                         case S:
                             return List.of(I2S);
+                        case OCTET:
+                            return List.of(I2B);
                         default:
-                            throw new IllegalArgumentException("Not handled:" + out);
+                            throw new IllegalArgumentException("Not handled, outputType=" + out);
                     }
                 case F:
                     switch (out) {
@@ -47,7 +49,7 @@ public class Cast implements Expression {
                         case L:
                             return List.of(F2L);
                         default:
-                            throw new IllegalArgumentException("Not handled:" + out);
+                            throw new IllegalArgumentException("Not handled, outputType=" + out);
                     }
                 case D:
                     switch (out) {
@@ -59,8 +61,10 @@ public class Cast implements Expression {
                             return List.of(D2F);
                         case L:
                             return List.of(D2L);
+                        case OCTET:
+                            return List.of(D2I, I2B);
                         default:
-                            throw new IllegalArgumentException("Not handled:" + out);
+                            throw new IllegalArgumentException("Not handled, outputType=" + out);
                     }
                 case L:
                     switch (out) {
@@ -72,12 +76,38 @@ public class Cast implements Expression {
                             return List.of(L2D);
                         case F:
                             return List.of(L2F);
+                        case OCTET:
+                            return List.of(L2I, I2B);
+                        default:
+                            throw new IllegalArgumentException("Not handled:" + out);
+                    }
+                case OCTET:
+                    switch (out) {
+                        case I:
+                        case L:
+                        case S:
+                            return List.of();
+                        default:
+                            throw new IllegalArgumentException("Not handled:" + out);
+                    }
+                case S:
+                    switch (out) {
+                        case I:
+                        case L:
+                        case S:
+                            return List.of();
+                        case D:
+                            return List.of(I2D);
+                        case F:
+                            return List.of(I2F);
+                        case OCTET:
+                            return List.of(I2B);
                         default:
                             throw new IllegalArgumentException("Not handled:" + out);
                     }
                 default:
                     // TODO
-                    throw new IllegalArgumentException("Oy vey, too many casts");
+                    throw new IllegalArgumentException("Internal error, unhandled cast, this should never happen, in=" + inputType + ", out=" + outputType);
             }
         }
         // TODO: Do I need to warn?
